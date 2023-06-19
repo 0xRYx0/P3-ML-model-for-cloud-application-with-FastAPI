b346855d76c657f34e869e431c6fc717dfe1ae33
Rakan <rakan210@gmail.com>
2023-06-20 01:00:39 +0300

    modifying model source code and testing scripts

---
+++ starter/model_card.md
@@ -0,0 +1,41 @@
+# Model Card
+
+## Overview
+
+This model is a classifier trained to predict whether an employer's income exceeds $50K/year. It was developed as part of the third project for Udacity's Machine Learning DevOps nanodegree in June 2023. The model utilizes the Random Forest algorithm.
+
+## Intended Use
+
+The model is intended to be used to determine what features impact the income of a person and identify underprivileged employers. It can provide insights into factors such as gender, race, and other features that may contribute to income disparities. However, it is important to note that the model may not be suitable for modern data analysis since the training data is relatively old.
+
+## Training Data
+
+The model was trained on the Census Income Dataset from the UCI Machine Learning Repository. The dataset consists of both categorical and numerical features. To preprocess the data, missing values in categorical features were imputed using the most frequent value, and the categories were encoded using `OrdinalEncoder`. Unknown categories were assigned a value of 1111. Numerical data was normalized using `StandardScaler`. The education column was dropped as it was already encoded in the education-num column.
+
+## Evaluation Data
+
+The evaluation data was obtained by splitting the training data using `train_test_split` from the `sklearn` library. The split was stratified based on the salary label, with a fixed random_state=42.
+
+## Metrics
+
+The model's performance was evaluated using `precision`, `recall`, and `F1` score. These metrics are suitable for imbalanced problems like this binary classification task. Precision represents the ratio of correct predictions to the total predictions, recall represents the ratio of correct predictions to the total number of correct items in the dataset, and F1 score is the harmonic mean between precision and recall, providing a balanced measure of both.
+
+## Ethical Considerations
+
+The data used in this model is open-sourced and available on the UCI Machine Learning Repository for educational purposes. However, it is important to note that the dataset was collected in 1996 and may not reflect insights from the modern world. When interpreting and using the model's results, it is essential to consider the limitations of the dataset's temporal relevance.
+
+## Caveats and Recommendations
+
+It is recommended to focus more on collecting additional data for features with minor categories to enhance the model's performance and coverage. As the training data is relatively old, it may not capture the dynamics of the modern world accurately. Therefore, caution should be exercised when applying the model to current scenarios.
+
+## Quantitative Analyses
+
+The following table shows the performance metrics calculated for class 1 (>50K) using sklearn metrics:
+
+|        | Train | Test  |
+|--------|-------|-------|
+| Precision | 0.6817 | 0.6210 |
+| Recall    | 0.9382 | 0.8359 |
+| F1        | 0.7896 | 0.7126 |
+
+These metrics indicate the model's precision, recall, and F1 score on both the training and test datasets.
---
+++ starter/testing/test_model.py
@@ -0,0 +1,102 @@
+import logging
+import os
+import pandas as pd
+
+from pipeline.data import clean_data
+from pipeline.model import create_pipeline
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+
+
+logging.basicConfig(filename=os.getcwd()+'/../logging.log', filemode='a', level=logging.INFO,
+                    format='%(asctime)s %(name)s %(levelname)s - %(message)s', datefmt="%m/%d/%y %I:%M:%S %p")
+
+
+def test_model_pipeline_creation():
+    """
+    Tests if model pipeline is created with correct configuration
+    """
+    # define main variables:
+    drop_columns = ['education']
+    numeric_columns = ['age','capital_gain','capital_loss','fnlgt', 'hours_per_week']
+    categorical_columns = ['marital_status', 'native_country', 'occupation',
+                          'relationship', 'race', 'sex', 'workclass']
+
+    # define grid prams
+    param_grid = {
+        'model__n_estimators': list(range(50, 251, 50)),
+        'model__max_depth': list(range(2, 15, 2)),
+        'model__min_samples_leaf': list(range(1, 51, 10))
+    }
+    
+    rf_model = RandomForestClassifier(class_weight='balanced', random_state=42)
+    model_pipe = create_pipeline(rf_model, categorical_columns, numeric_columns, drop_columns)
+    
+    
+    # check classifer 
+    assert isinstance(model_pipe[1], RandomForestClassifier), "Model is not Random Forest Classifier"
+        
+    # check features columns     
+    assert model_pipe[0].transformers[0][2] == drop_columns, "Dropped cloumn is mismatched"
+    assert model_pipe[0].transformers[1][2] == numeric_columns, "Numeric cloumns are mismatched" 
+    assert model_pipe[0].transformers[2][2] == categorical_columns, "Categorical cloumns are mismatched"  
+    
+    # check numerical and categorical encoders        
+    assert isinstance(model_pipe[0].transformers[1][1], StandardScaler), "Numeric encoder is not StandardScaler"  
+    assert isinstance(model_pipe[0].transformers[2][1][1], OrdinalEncoder), "Categorical encoder is not OrdinalEncoder"    
+    print("SECCESS: Model pipeline was formed correctly")
+        
+    
+
+def test_data_shape_match():
+    """
+    Tests if dataset shape are matched
+    """
+    
+    X,y = clean_data(os.getcwd()+'/../data/census.csv')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+    
+    assert X_train.shape[0] == y_train.shape[0], "Shape mismatch between X_train and y_train"
+    assert X_test.shape[0] == y_test.shape[0], "Shape mismatch between X_test and y_test"
+    assert X_train.shape[1] == X_test.shape[1], "Number of features mismatch between X_train and X_test"
+    print("SECCESS: Shapes of sliced data are matching")
+
+
+
+def assert_dataset_requirements():
+    """
+    Tests if dataset requirements were satisfied 
+    """
+    
+    X,y = clean_data(os.getcwd()+'/../data/census.csv')
+    df = pd.concat([X,y], axis=1)
+
+    # expected columns and data types after processing  
+    required_columns = {
+        'age': 'int64',
+        'capital_gain': 'int64',
+        'capital_loss': 'int64',
+        'hours_per_week': 'int64',
+        'education_num': 'int64',
+        'fnlgt': 'int64',
+        'education': 'object',
+        'marital_status': 'object',
+        'occupation': 'object',
+        'relationship': 'object',
+        'race': 'object',
+        'sex': 'object',
+        'native_country': 'object',
+        'salary': 'int64',
+        'workclass': 'object'
+    }
+    
+    # Check if all required columns are present in the dataset
+    assert set(required_columns.keys()).issubset(df.columns), "Missing required columns in the dataset"
+    print("SECCESS: Required column were set")
+    
+    # Check if the data types of required columns match the expected types
+    for column, expected_dtype in required_columns.items():
+        assert df[column].dtype == expected_dtype, f"Column '{column}' has incorrect data type: expected {expected_dtype} got {df[column].dtype}"
+    print("SECCESS: Required column and data types were set")
