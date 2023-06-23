23dff71c4b0bfa7621a2b5d24ba007a866e8b5fd
Rakan <rakan210@gmail.com>
2023-06-23 14:02:18 +0300

    Modify Git Actions

--- api.py
+++ api.py
@@ -22,55 +22,53 @@ from pydantic import BaseModel
 sys.path.append(os.getcwd()+'/starter')
 from directories import _API_APP_CONFIGURATION, _MODEL_CONFIGURATION
 
-# # Create FastAPI instance
-# app = FastAPI()
+# Create FastAPI instance
+app = FastAPI()
 
-# # Loading configurations
-# with open(_API_APP_CONFIGURATION) as fp:
-#         config = yaml.safe_load(fp)
+# Loading configurations
+with open(_API_APP_CONFIGURATION) as fp:
+        config = yaml.safe_load(fp)
         
-# # Model input data schema
-# class InputData(BaseModel):
-#     age: int
-#     capital_gain: int
-#     capital_loss: int
-#     fnlgt: int
-#     education_num: int
-#     hours_per_week: int
-#     workclass: str = None
-#     education: str = None
-#     marital_status: str = None
-#     occupation: str = None
-#     relationship: str = None
-#     race: str = None
-#     sex: str = None
-#     native_country: str = None
+# Model input data schema
+class InputData(BaseModel):
+    age: int
+    capital_gain: int
+    capital_loss: int
+    fnlgt: int
+    education_num: int
+    hours_per_week: int
+    workclass: str = None
+    education: str = None
+    marital_status: str = None
+    occupation: str = None
+    relationship: str = None
+    race: str = None
+    sex: str = None
+    native_country: str = None
 
-# # Loading required model
-# model = joblib.load(_MODEL_CONFIGURATION)
+# Loading required model
+model = joblib.load(_MODEL_CONFIGURATION)
 
+# GET endpoint for root
+@app.get("/")
+async def index():
+    logging.info("SUCCESS: API route for - Index Endpoint") 
+    return {"message": "Here we go! Welcome"}
 
-# # GET endpoint for root
-# @app.get("/")
-# async def index():
-#     logging.info("SUCCESS: API route for - Index Endpoint") 
-#     return {"message": "Here we go! Welcome"}
+# GET endpoint for features
+@app.get("/features_details/{feature}")
+async def feature_info(feature):
+    info = config['features_details'][feature]
+    return info
 
-# # GET endpoint for features
-# @app.get("/features_details/{feature}")
-# async def feature_info(feature):
-#     info = config['features_details'][feature]
-#     return info
-
-
-# # POST endpoint for model inference
-# @app.post("/prediction")
-# async def inference(input_data: InputData = Body(...,examples=config['post_examples'])):
+# POST endpoint for model inference
+@app.post("/prediction")
+async def inference(input_data: InputData = Body(...,examples=config['post_examples'])):
         
-#     features = np.array([input_data.__dict__[f] for f in config['features_details']])
-#     features = pd.DataFrame(features.reshape(1, -1), columns=config['features_details'])
-#     predicted_label = int(model.predict(features))
-#     prediction_probability = float(model.predict_proba(features)[:, 1])
-#     pred = '>50k' if predicted_label == 1 else '<=50k'
+    features = np.array([input_data.__dict__[f] for f in config['features_details']])
+    features = pd.DataFrame(features.reshape(1, -1), columns=config['features_details'])
+    predicted_label = int(model.predict(features))
+    prediction_probability = float(model.predict_proba(features)[:, 1])
+    pred = '>50k' if predicted_label == 1 else '<=50k'
 
-#     return {'Prediction': predicted_label, 'Probability': prediction_probability, 'Salary Range': pred}
+    return {'Prediction': predicted_label, 'Probability': prediction_probability, 'Salary Range': pred}
