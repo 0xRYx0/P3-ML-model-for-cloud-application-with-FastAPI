e88a86f236792299f78e5f8ecb0b4c7fa584abd4
Rakan <rakan210@gmail.com>
2023-07-06 22:47:59 +0300

    modifying fastapi code

--- fastapi_app.py
+++ fastapi_app.py
@@ -63,7 +63,7 @@ async def feature_info(feature):
 
 # POST endpoint for model inference
 @app.post("/prediction/")
-async def prediction(input_data: InputData = Body(..., examples=configurations['fastapi_post_examples'])):
+async def prediction(input_data: Annotated[InputData,Body(..., examples=configurations['fastapi_post_examples'])]):
         
     print('1.1 start function')    
     features = np.array([input_data.__dict__[f] for f in configurations['fastapi_features_details']])
