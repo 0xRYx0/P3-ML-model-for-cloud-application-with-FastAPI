bb3272d37d2ccff50a1abd1ac5cd894ab802ffba
Rakan <rakan210@gmail.com>
2023-07-06 22:04:48 +0300

    modifying fastapi code

--- fastapi_app.py
+++ fastapi_app.py
@@ -47,10 +47,29 @@ class InputData(BaseModel):
     sex: str = None
     native_country: str = None
     
-    class Config:
-        schema_extra = {
-            "examples": {
-                "class >50k [label 1]": {
+
+# GET endpoint for root
+@app.get("/")
+async def index():
+    logging.info("SUCCESS: API route for - Index Endpoint") 
+    return {"message": "Here we go! Welcome"}
+
+# GET endpoint for features
+@app.get("/features_details/{feature}")
+async def feature_info(feature):
+    info = configurations['fastapi_features_details'][feature]
+    return info
+
+
+# POST endpoint for model inference
+@app.post("/prediction/")
+async def inference(
+    *,
+    input_data: Annotated [
+        InputData,
+        Body(
+            example = [
+                {
                     "summary": "An example of class >50k",
                     "description": "A example that should predict a class of >50k",
                     "value": {
@@ -70,7 +89,7 @@ class InputData(BaseModel):
                         "native_country": "taiwan",
                     },
                 },
-                "class <=50k [label 0]": {
+                {
                     "summary": "An example of an individual with an income of <=50k",
                     "description": "This example represents an individual with various characteristics that are used to predict their income level",
                     "value": {
@@ -90,7 +109,7 @@ class InputData(BaseModel):
                         "native_country": "united-states",
                     },
                 },
-                "error_sample": {
+                {
                     "summary": "An example of a sample that will cause an error",
                     "description": "This example represents a sample that will cause an error with the model due to missing age and fnlgt variables",
                     "value": {
@@ -108,7 +127,7 @@ class InputData(BaseModel):
                         "native_country": "taiwan"
                     },
                 },
-                "missing_sample": {
+                {
                     "summary": "An example of a sample with missing values",
                     "description": "This example showcases the model's ability to handle missing values for certain features",
                     "value": {
@@ -120,26 +139,8 @@ class InputData(BaseModel):
                         "hours_per_week": 1
                     },
                 },
-            }
-        }
-
-
-# GET endpoint for root
-@app.get("/")
-async def index():
-    logging.info("SUCCESS: API route for - Index Endpoint") 
-    return {"message": "Here we go! Welcome"}
-
-# GET endpoint for features
-@app.get("/features_details/{feature}")
-async def feature_info(feature):
-    info = configurations['fastapi_features_details'][feature]
-    return info
-
-
-# POST endpoint for model inference
-@app.post("/prediction/")
-async def inference(input_data: Annotated [InputData,Body(None,examples=InputData.Config.schema_extra["examples"])]):
+            ]
+)]):
         
     print('1.1 start function')    
     features = np.array([input_data.__dict__[f] for f in configurations['features_details']])
