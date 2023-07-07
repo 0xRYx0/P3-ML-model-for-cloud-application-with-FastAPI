7e2409d6fe20d7c23a68883f5236cd7a34de58ae
Rakan <rakan210@gmail.com>
2023-07-07 10:13:52 +0300

    API commit

--- starter/src/app/api.py
+++ starter/src/app/api.py
@@ -7,7 +7,6 @@
   Date    :  19 June 2023
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
-from fastapi import FastAPI, Body
 import joblib
 import os
 import yaml
@@ -16,7 +15,7 @@ import logging
 import numpy as np
 import pandas as pd
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel
 
 sys.path.append(os.getcwd()+'/../..')
@@ -25,19 +24,23 @@ from directories import _API_APP_CONFIGURATION, _MODEL_CONFIGURATION
 # Create FastAPI instance
 app = FastAPI()
 
-# # Loading configurations
-# with open(_API_APP_CONFIGURATION) as fp:
-#         config = yaml.safe_load(fp)
+# # # Loading configurations
+# # with open(_API_APP_CONFIGURATION) as fp:
+# #         config = yaml.safe_load(fp)
 
-# Loading required model
-# print('######### MODEL: '+_MODEL_CONFIGURATION)
-# model = joblib.load(_MODEL_CONFIGURATION)
-        
+# #Loading required model
+# # print('######### MODEL: '+_MODEL_CONFIGURATION)
+# # model = joblib.load(_MODEL_CONFIGURATION)
+
+# config_path = os.getcwd()+'/starter/src/app/app_config.yaml'
+# model_path = 'app_model.pkl'    
+
 # Loading configurations
-with open("app_config.yaml") as fp:
-    config = yaml.safe_load(fp)
-    
-model = joblib.load("app_model.pkl")
+with open(_API_APP_CONFIGURATION) as fp:
+    configurations = yaml.safe_load(fp)
+        
+# # Loading required model
+# model = joblib.load(_MODEL_CONFIGURATION)
         
 # Model input data schema
 class InputData(BaseModel):
@@ -66,23 +69,23 @@ async def index():
 # GET endpoint for features
 @app.get("/features_details/{feature}")
 async def feature_info(feature):
-    info = config['features_details'][feature]
+    info = configurations['features_details'][feature]
     return info
 
 
-# POST endpoint for model inference
-@app.post("/predictions/")
-async def inference(input_data: InputData = Body(...,examples=config['post_examples'])):
-    print('1. start function')    
-    features = np.array([input_data.__dict__[f] for f in config['features_details']])
-    print('2. start features')  
-    features = pd.DataFrame(features.reshape(1, -1), columns=config['features_details'])
-    print('3. Reshape features')  
-    predicted_label = int(model.predict(features))
-    print('4. predicted_label')  
-    prediction_probability = float(model.predict_proba(features)[:, 1])
-    print('5. prediction_probability')  
-    pred = '>50k' if predicted_label == 1 else '<=50k'
-    print('. prediction_probability') 
+# # POST endpoint for model inference
+# @app.post("/predictions/")
+# async def inference(input_data: InputData = Body(...,examples=configurations['post_examples'])):
+#     print('1. start function')    
+#     features = np.array([input_data.__dict__[f] for f in configurations['features_details']])
+#     print('2. start features')  
+#     features = pd.DataFrame(features.reshape(1, -1), columns=configurations['features_details'])
+#     print('3. Reshape features')  
+#     predicted_label = int(model.predict(features))
+#     print('4. predicted_label')  
+#     prediction_probability = float(model.predict_proba(features)[:, 1])
+#     print('5. prediction_probability')  
+#     pred = '>50k' if predicted_label == 1 else '<=50k'
+#     print('. prediction_probability') 
 
-    return {'Prediction': predicted_label, 'Probability': prediction_probability, 'Salary Range': pred}
+#     return {'Prediction': predicted_label, 'Probability': prediction_probability, 'Salary Range': pred}
