c238cffff70d22c3ecb210b0ec444f1673a68665
Rakan <rakan210@gmail.com>
2023-07-07 12:54:30 +0300

    modify main script

--- starter/src/app/api.py
+++ starter/src/app/api.py
@@ -65,18 +65,26 @@ async def feature_info(feature):
 # POST endpoint for model inference
 @app.post("/predictions/")
 async def inference(input_data: InputData = Body(...,examples=configurations['post_examples'])):
-    print('1. start function')    
-    features = np.array([input_data.__dict__[f] for f in configurations['features_details']].keys())
-    print(features)
-    print('2. start features')  
-    df_features = pd.DataFrame(features.reshape(1, -1), columns=configurations['features_details'].keys())
-    print(features)
-    print('3. Reshape features')  
-    predicted_label = int(model.predict(df_features))
-    print('4. predicted_label')  
-    prediction_probability = float(model.predict_proba(df_features)[:, 1])
-    print('5. prediction_probability')  
-    pred = '>50k' if predicted_label == 1 else '<=50k'
-    print('. prediction_probability') 
+    # print('1. start function')    
+    # features = np.array([input_data.__dict__[f] for f in configurations['features_details']].keys())
+    # print(features)
+    # print('2. start features')  
+    # df_features = pd.DataFrame(features.reshape(1, -1), columns=configurations['features_details'].keys())
+    # print(features)
+    # print('3. Reshape features')  
+    # predicted_label = int(model.predict(df_features))
+    # print('4. predicted_label')  
+    # prediction_probability = float(model.predict_proba(df_features)[:, 1])
+    # print('5. prediction_probability')  
+    # pred = '>50k' if predicted_label == 1 else '<=50k'
+    # print('. prediction_probability') 
+    
+    
+    data = input_data.dict()
+    features = np.array([data[f] for f in configurations['features_details'].keys()]).reshape(1, -1)
+    df = pd.DataFrame(features, columns=configurations['features_info'].keys())
+    pred_label = int(model.predict(df))
+    pred_probs = float(model.predict_proba(df)[:, 1])
+    pred = '>50k' if pred_label == 1 else '<=50k'
 
-    return {'Prediction': predicted_label, 'Probability': prediction_probability, 'Salary Range': pred}
+    return {'Prediction': pred_label, 'Probability': pred_probs, 'Salary Range': pred}
